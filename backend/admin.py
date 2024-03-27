9484e7c3df163928657d8d7a4189acda8a4eb903
Louiz Hounas <hlouiz@yandex.ru>
2024-03-27 17:40:19 +0300

    Admin panel: creation

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from backend.models import User, Order, Shop, Product
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'company', 'position', 'type', 'created_at', 'updated_at')
+    list_filter = ('type', 'position', 'company')
+    files = ('username', 'first_name', 'last_name', 'email', 'company', 'position', 'type', 'created_at', 'updated_at')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'state', 'created_at', 'updated_at')
+    list_filter = ('state', 'user')
+    files = ('user', 'state', 'created_at', 'updated_at')
+    search_fields = ('user', 'state')
+
+
+# ToDo: Зарегистрировать все остальные модели
+
+admin.site.register(Shop)
+admin.site.register(Product)
