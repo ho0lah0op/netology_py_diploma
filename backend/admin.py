cf2d8e1946c2764682660dd436e31a2d66b1cf3f
Louiz Hounas <hlouiz@yandex.ru>
2024-03-28 09:56:24 +0300

    Admin panel: normalization 2

--- backend/admin.py
+++ backend/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from backend.models import User, Order, Shop, Product
+from backend.models import (
+    User, Order, Shop, Product, Category, ProductInfo, Parameter,
+    ProductParameter, Contact, OrderItem, ConfirmEmailToken
+)
 
 
 @admin.register(User)
@@ -21,11 +24,73 @@ class OrderAdmin(admin.ModelAdmin):
 
 
 # ToDo: Зарегистрировать все остальные модели
-# @admin.register(Shop)
-# class ShopAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'url', 'user', 'state', 'created_at', 'updated_at')
-#     list_filter = ('state', 'user')
-#     files = ('name', 'url', 'user', 'state', 'created_at', 'updated_at')
-#     search_fields = ('name', 'url', 'user')
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url', 'user', 'state')
+    list_filter = ('state', 'user')
+    files = ('name', 'url', 'user', 'state')
+    search_fields = ('name', 'url', 'user')
 
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'created_at', 'updated_at')
+    list_filter = ('name', 'category')
+    files = ('name', 'category', 'created_at', 'updated_at')
+    search_fields = ('name', 'category')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    files = ('name', 'shops')
+    search_fields = ('name',)
+
+
+@admin.register(ProductInfo)
+class ProductInfoAdmin(admin.ModelAdmin):
+    list_display = ('product', 'model', 'quantity', 'price')
+    list_filter = ('product', 'model')
+    files = ('product', 'model', 'quantity', 'price')
+    search_fields = ('product', 'model')
+
+
+@admin.register(Parameter)
+class ParameterAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    files = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(ProductParameter)
+class ProductParameterAdmin(admin.ModelAdmin):
+    list_display = ('product_info', 'parameter', 'value')
+    list_filter = ('product_info', 'parameter')
+    files = ('product_info', 'parameter', 'value')
+    search_fields = ('product_info', 'parameter')
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('user', 'city', 'street', 'house', 'structure', 'building', 'apartment', 'phone')
+    list_filter = ('user', 'city', 'phone')
+    files = ('user', 'city', 'street', 'house', 'structure', 'building', 'apartment', 'phone')
+    search_fields = ('user',)
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product_info', 'quantity')
+    list_filter = ('order', 'product_info')
+    files = ('order', 'product_info', 'quantity')
+    search_fields = ('order', 'product_info')
+
+
+@admin.register(ConfirmEmailToken)
+class ConfirmEmailAdmin(admin.ModelAdmin):
+    list_display = ('user', 'key', 'created_at')
+    list_filter = ('user',)
+    files = ('user', 'key', 'created_at')
+    search_fields = ('user',)
