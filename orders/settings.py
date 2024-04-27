f25f404ceabb730c2a59dc58cbdc96911f2c7a57
Louiz Hounas <hlouiz@yandex.ru>
2024-04-27 12:46:02 +0300

    DRF Throttling

--- orders/settings.py
+++ orders/settings.py
@@ -146,6 +146,15 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/minute',
+        'user': '100/minute'
+    }
 }
 
 SPECTACULAR_SETTINGS = {
